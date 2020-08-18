82303bb561404722207805f80e25a31f0485b08b
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-08-18 10:10:00 -0500

    add !play

--- RFA.js
+++ RFA.js
@@ -70,22 +70,35 @@ client.on('message', msg => {
 
 
 client.on('message', async voice => {
+
   // Join the same voice channel of the author of the message
-  if (voice.content == `${prefix}play`) {
+  if (voice.content.includes(`${prefix}play`)) {
 	  if (voice.member.voice.channel) {
       const connection = await voice.member.voice.channel.join();
       
       const fs = require('fs');
       const ytdl = require('ytdl-core-discord');
-    
+
+      const { apiurl } = require('ytsearcher');
+
+      const { YTSearcher } = require('ytsearcher');
+      const searcher = new YTSearcher('AIzaSyAvFzxIPtrBdOPe5YIEGohywE4EEDLVQwU');
+
+      var input = voice.content;
+      var usrInput = input.substr('6');
+      let result = await searcher.search(usrInput);
+      //voice.channel.send(usrInput + '\n' + result.first.url);
+
       // Create a dispatcher
+
       //const dispatcher = connection.play('https://www.youtube.com/watch?v=xGtyOPC3mEA'); , { quality: 'highestaudio' }
-      const dispatcher = connection.play(await ytdl('https://www.youtube.com/watch?v=xGtyOPC3mEA'), { type: 'opus' }, {quality: 'lowest' });
+      //const dispatcher = connection.play(await ytdl('https://www.youtube.com/watch?v=xGtyOPC3mEA'), { type: 'opus' }, {quality: 'lowest' });
       //https://www.youtube.com/watch?v=HRW9W7ZtOEI
       //const dispatcher = connection.play(await ytdl('https://www.youtube.com/watch?v=HRW9W7ZtOEI'), { type: 'opus' }, {quality: 'lowest' });
+      const dispatcher = connection.play(await ytdl(result.first.url), { type: 'opus' }, {quality: 'lowest' });
 
       dispatcher.on('start', () => {
-	      voice.channel.send('Song is now playing!');
+	      voice.channel.send('Now playing ' + result.first.url);
       });
 
       dispatcher.on('finish', () => {
