d903a29663402e22a87df4e31c0d049e1aee18a8
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-08-26 15:43:21 -0500

    r!mature command (90% complete)

--- commands/immature.js
+++ commands/immature.js
@@ -1,33 +1,44 @@
 module.exports = {
-	name: 'immature',
+	name: 'mature',
 	description: '',
 	execute(msg, args) {
 
-        const user = msg.client.users.cache.get(msg.author.id);
-        user.send("nico-nico-nii https://www.youtube.com/watch?v=T4iC52d-fVY");
+        //DM Matthew and Thomas when command is invoked
+        const matthew = msg.client.users.cache.get(`195651503673835520`);
+        const thomas = msg.client.users.cache.get(`183339913008119808`);
+        const chris = msg.client.users.cache.get(`279032930926592000`);
+        //matthew.send("\**Mature Role:\** Press on the reaction to confirm role\nThis will timeout in 20 minutes.");
+        //thomas.send("\**Mature Role:\** Press on the reaction to confirm role\nThis will timeout in 20 minutes.");
 
-        msg.react('👍');
+        //reacts with MonkaS (custom reaction) (must be in the same server that the command is called from)
+        const reactionEmoji = msg.guild.emojis.cache.find(emoji => emoji.name === 'monkaS');
+        msg.react(reactionEmoji);
 
+        //(user.id === `183339913008119808` || user.id === `279032930926592000` || user.id === `195651503673835520`)
         const filter = (reaction, user) => {
-            return reaction.emoji.name === '👍' && (user.id === `183339913008119808` || user.id === `279032930926592000`);
+            return reaction.emoji.name === `monkaS` && (user.id == chris || user.id == thomas || user.id == matthew);
         };
-        
-        const collector = msg.createReactionCollector(filter, { time: 15000 });
+        //1200000
+        const collector = msg.createReactionCollector(filter, { time: 1200000 });
         
         collector.on('collect', (reaction, user) => {
-            msg.channel.send(`${msg.author} is very immature`);
+
+            //Runs if filter detects reaction from [insert user name here]
+            msg.channel.send(`\**Mature Role:\** ${msg.author} now has the \`@mature\` and \`@mature music\` roles`);
             const immature = msg.member.guild.roles.cache.get('746601135149809684');
             const immaturemusic = msg.guild.roles.cache.get(`615191480067293210`);
             msg.member.roles.add(immature);
             msg.member.roles.add(immaturemusic);
+            msg.channel.send(`It worked`);
         });
         
         collector.on('end', collected => {
 
+            console.log(collected.size);
+
+            //Runs when ReactionCollector timesout and if no reactions were found
             if (collected.size == 0) {
-                msg.channel.send(`\**Error:\** Timeout, try again!`);
-            } else {
-                msg.channel.send(`Cooldown period has ended`);
+                msg.channel.send(`\**Mature Role:\** ${msg.author} role request timed out! Try again later`);
             }
 
         });
