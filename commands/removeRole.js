67de0b8638420f4cc0e74d23ec3876ee85ab5fdf
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-08-26 17:33:34 -0500

    removeRole updates

--- commands/removeRole.js
+++ commands/removeRole.js
@@ -1,12 +1,23 @@
+const { User } = require("discord.js");
+
 module.exports = {
     name: 'removeRole',
     description: '',
     execute(msg, args) {
 
-        
+        //Declare Constants
+        const muted = '611043295279316992';
 
+        //Check to see if msg.author is muted
+        if (msg.member.roles.cache.has(muted)) {
 
+            msg.channel.send(`\**Error:\** You are muted!`);
+            
+        } else {
 
+            
+
+        }
 
     },
 
