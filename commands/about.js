59c9a293210617c8a7a65a01dd9fb331534f99db
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-08-27 15:28:52 -0500

    added about command

---
+++ commands/about.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'about',
+    description: '',
+    execute(msg, args) {
+
+        const aboutEmbed = new Discord.MessageEmbed()
+        .setTitle(`About Rebel Fleet Admiral Bot`)
+        .setURL(`https://chr1s.dev`)
+        .setAuthor("About!", "https://cdn.discordapp.com/icons/586736904771469313/0e4b24f8252d25949976c14b9ce5c11c.webp","https://chr1s.dev")
+        .setColor('#02f2ce')
+        .setDescription(`\**Rebel Fleet Admiral\** (RFA) is an all-purpose discord bot for the discord server "Rebel Grenades". It's main purpose is moderation and music. But, it also includes other miscellaneous commands for obtaining information. Such as member profile link and guild member count.`)
+        .setFooter(`Developer: Chr1sDev (クリス)`)
+        .setThumbnail(`https://cdn.discordapp.com/icons/586736904771469313/0e4b24f8252d25949976c14b9ce5c11c.webp`)
+        msg.channel.send(aboutEmbed)
+
+    },
+}
