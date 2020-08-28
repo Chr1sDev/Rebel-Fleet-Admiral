a3ee3f70aae7afbeca1ecd912a27428d323d2fad
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-08-28 18:56:09 -0500

    update about

--- commands/about.js
+++ commands/about.js
@@ -10,7 +10,7 @@ module.exports = {
         .setAuthor("About!", "https://cdn.discordapp.com/icons/586736904771469313/0e4b24f8252d25949976c14b9ce5c11c.webp","https://chr1s.dev")
         .setColor('#02f2ce')
         .setDescription(`\**Rebel Fleet Admiral\** (RFA) is an all-purpose discord bot for the discord server "Rebel Grenades". It's main purpose is moderation and music. But, it also includes other miscellaneous commands for obtaining information. Such as member profile link and guild member count.`)
-        .setFooter(`Developer: Chr1sDev (クリス)`)
+        .setFooter(`Developer: Chr1sDev (クリス)`, `https://chr1s.dev/assets/verified_dev.png`)
         .setThumbnail(`https://cdn.discordapp.com/icons/586736904771469313/0e4b24f8252d25949976c14b9ce5c11c.webp`)
         msg.channel.send(aboutEmbed)
 
