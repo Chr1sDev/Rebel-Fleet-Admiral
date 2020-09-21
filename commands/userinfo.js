14c1a7a00a8a3ad6d9765d3df1b0b7647832267a
Christopher <32487758+Ra1nDev@users.noreply.github.com>
2020-09-21 12:26:04 -0500

    add stuff

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'userinfo',
 	description: '',
 	execute(msg, args) {
-        msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
+        msg.channel.send(`\**Your username:\** ${msg.author.username}\n\**Your ID:\** ${msg.author.id}\n\**Date Created:\** ${msg.author.createdAt}`);
 	},
 };
